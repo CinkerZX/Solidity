74d128b3b282c561b3c38b0cb01b2cff1b44e56c
CinkerZX <sindy_zhou126@126.com>
2024-05-23 12:46:31 +0200

    Use modifier to integrate time framework, and update the compile's version, codes are adapted accordingly

--- contracts/Week3/Ballot3.sol
+++ contracts/Week3/Ballot3.sol
@@ -1,5 +1,4 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.4.0;
+pragma solidity ^0.8.25;
 
 contract Ballot{
 
@@ -23,38 +22,47 @@ contract Ballot{
 
     uint startTime;
     //modifiers
+    modifier validStage(Stage reqStage){
+        require(stage == reqStage);
+        _;
+    }
+
+    event votingCompleted();
 
     // Constructor: Create a new ballot with n proposals, namely there are n candidates
-    function Ballot(uint8 _numProposals) public {
+    constructor(uint8 _numProposals) {
         chairperson = msg.sender;
         voters[chairperson].weight = 2; // the weight of chairperson is 2
-        proposals.length = _numProposals;
+        for(uint i=0;i<_numProposals;i++)
+            proposals.push(Proposal(0));
         stage = Stage.Reg;
-        startTime = now;
+        startTime = block.timestamp;
     }
 
     // Initialize as a valid voter (not the chairperson and never voted before
-    function register(address toVoter) public{
-        if(stage != Stage.Reg) {return;} // add a statu check
+    function register(address toVoter) public validStage(Stage.Reg){
+        // if(stage != Stage.Reg) {return;} // add a statu check
         if(msg.sender!= chairperson || voters[toVoter].voted) {return;}
         voters[toVoter].weight = 1;
         voters[toVoter].voted = false;
-        if (now >(startTime + 10 seconds)){stage = Stage.Vote; startTime = now;}
+        if (block.timestamp >(startTime + 10 seconds)){stage = Stage.Vote; startTime = block.timestamp;}
     }
 
     // A sender vote for candidate 'toProposal'
-    function vote(uint8 toProposal) public {
-        if (stage != Stage.Vote) {return;}
+    function vote(uint8 toProposal) public validStage(Stage.Vote){
+        // if (stage != Stage.Vote) {return;}
         Voter storage sender = voters[msg.sender];
         if (sender.voted || toProposal >= proposals.length) return; // if the sender voted before or vote for an invalid candidate, cannot vote
         sender.voted = true;
         sender.vote = toProposal;
         proposals[toProposal].voteCount += sender.weight;
-        if (now>(startTime + 10 seconds)) {stage = Stage.Done;}
+        if (block.timestamp>(startTime + 10 seconds)) {
+            stage = Stage.Done;
+            emit votingCompleted();}
     }
 
-    function winningProposal() public constant returns (uint _winningProposal){
-        if(stage != Stage.Done){return;}
+    function winningProposal() public validStage(Stage.Done) view returns (uint _winningProposal){
+        // if(stage != Stage.Done){return;}
         uint256 winningVoteCount = 0;
         for (uint8 prop = 0; prop < proposals.length; prop++) 
         {
@@ -63,7 +71,8 @@ contract Ballot{
                 _winningProposal = prop;
             }
         }
-        return _winningProposal;
+        // return _winningProposal;
+        assert(winningVoteCount > 0);
     }
 
 }
